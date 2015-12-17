dcb228a74a9e5b9aed66e34c23fbca2d0c94ac5d
Robin Kunde <robin.kunde@recoursive.com>
2015-12-17 17:19:45 -0500

    revision pass for electricsparkscript

--- UnityProject/Assets/Scripts/electricsparkscript.js
+++ UnityProject/Assets/Scripts/electricsparkscript.js
@@ -1,33 +1,41 @@
+/*
+This script controlls the behavior of the electrical sparks emitted by destroyed drones and turrets.
+
+General notes:
+1. opac var is used to both set the color/opacity of the spark as well as determine its lifetime.
+*/
+
 #pragma strict
 
 var opac = 0.0;
 
-function UpdateColor() {
-	var renderers = transform.GetComponentsInChildren(MeshRenderer);
-	var color = Vector4(opac,opac,opac,opac);
-	for(var renderer : MeshRenderer in renderers){
-		renderer.material.SetColor("_TintColor", color);
-	}
-	var lights = transform.GetComponentsInChildren(Light);
-	for(var light : Light in lights){
-		light.intensity = opac * 2.0;
-	}
+function Start() {
+    // The color vector uses values between 0 and 1 for RGBA.
+    opac = Random.Range(0.4, 1.0);
+    UpdateColor();
+    // Set random z axis orientation.
+    transform.localRotation.eulerAngles.z = Random.Range(0.0, 360.0);
+    // Randomly scale spark size, along each axis individually.
+    transform.localScale.x = Random.Range(0.8, 2.0);
+    transform.localScale.y = Random.Range(0.8, 2.0);
+    transform.localScale.z = Random.Range(0.8, 2.0);
 }
 
-function Start () {
-	opac = Random.Range(0.4,1.0);
-	UpdateColor();
-	transform.localRotation.eulerAngles.z = Random.Range(0.0,360.0);
-	transform.localScale.x = Random.Range(0.8,2.0);
-	transform.localScale.y = Random.Range(0.8,2.0);
-	transform.localScale.z = Random.Range(0.8,2.0);
+function UpdateColor() {
+    var color = Vector4(opac, opac, opac, opac);
+    for (var renderer : MeshRenderer in transform.GetComponentsInChildren(MeshRenderer)) {
+        renderer.material.SetColor("_TintColor", color);
+    }
+    for (var light : Light in transform.GetComponentsInChildren(Light)) {
+        light.intensity = opac * 2.0;
+    }
 }
 
 function Update() {
-	UpdateColor();
-	opac -= Time.deltaTime * 5.0;
-	transform.localScale += Vector3(1,1,1)*Time.deltaTime*30.0;
-	if(opac <= 0.0){
-		Destroy(gameObject);
-	}
+    UpdateColor();
+    opac -= Time.deltaTime * 5.0;
+    transform.localScale += Vector3(1, 1, 1) * Time.deltaTime * 30.0;
+    if (opac <= 0.0) {
+        Destroy(gameObject);
+    }
 }

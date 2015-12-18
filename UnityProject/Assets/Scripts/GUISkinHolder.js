8831c61cecb9650b88b02eff5a1f8fb532df7f02
Robin Kunde <robin.kunde@recoursive.com>
2015-12-18 17:14:04 -0500

    revision pass for GUISkinHolder

--- UnityProject/Assets/Scripts/GUISkinHolder.js
+++ UnityProject/Assets/Scripts/GUISkinHolder.js
@@ -1,21 +1,32 @@
 #pragma strict
 
-var gui_skin:GUISkin;
+// outlets
+
+var gui_skin : GUISkin;
+
 var sound_scream : AudioClip[];
-var sound_tape_content : AudioClip[];
-var sound_tape_start : AudioClip;
-var sound_tape_end : AudioClip;
+
+var sound_tape_content    : AudioClip[];
+var sound_tape_start      : AudioClip;
+var sound_tape_end        : AudioClip;
 var sound_tape_background : AudioClip;
-var tape_object : GameObject;
+var tape_object           : GameObject;
+
 var win_sting : AudioClip;
+
 var weapons : GameObject[];
-var weapon : GameObject;
+
 var flashlight_object : GameObject;
+
+// state
+
+@HideInInspector
 var has_flashlight = false;
+@HideInInspector
+var weapon : GameObject;
 
 function Awake () {
-	//weapon = weapons[2];
-	weapon = weapons[Random.Range(0,weapons.length)];
+	weapon = weapons[Random.Range(0, weapons.length)];
 }
 
 function Start () {
